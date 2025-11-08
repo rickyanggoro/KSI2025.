dc4a766890d898e77ed7794762e22c0348bfa63e
Ricky Anggoro <ricky.mdksvc2233@gmail.com>
2025-11-08 21:46:49 +0700

    Menambahkan fitur tambah data (tambah_data.php)

---
+++ tambah_data.php
@@ -0,0 +1,14 @@
+<?php
+include 'koneksi.php';
+
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO users (nama, email) VALUES ('$nama', '$email')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Data berhasil ditambahkan!";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+?>
